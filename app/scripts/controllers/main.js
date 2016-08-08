086784d207a8078fe98c284757f5d7c872ccbbb9
Sumeer Tuli <sumeertuli7@gmail.com>
2016-08-07 22:36:36 -0700

    right column movie crew

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,18 +16,76 @@ function MainController(movieDtlsService) {
     var mainVm = this;
 		mainVm.parts=[];
 		mainVm.log=[];
-      movieDtlsService.getCollection().then(function (result) {
-	  console.log('result', result);
-	  mainVm.parts = result.parts;
-	  console.log('parts', mainVm.parts);
-	  angular.forEach(mainVm.parts, function(value, key) {
-		  this.push(value.id);
-		}, mainVm.log);
-	  console.log('log', mainVm.log);
-	  
-	  
-	}, function (error) {
-	  console.log('err', error.status);
-	});
+		mainVm.movieDtlsList=[];
+		mainVm.movieCreditsList=[];
+		mainVm.movieDt ={};
 
-  }
+
+  		movieDtlsService.getCollection().then(function (result) {
+	  		// console.log('result', result);
+	  	mainVm.parts = result.parts;
+	  		// console.log('parts', mainVm.parts);
+
+	  	mainVm.parts.director;
+	  	angular.forEach(mainVm.parts, function(value, key) {
+		  this.push(value.id);
+		  	// console.log('key', key);
+		  	// console.log('value', this[key]);
+
+		  	(function(value) {
+		  		movieDtlsService.getMovieById(value)
+		  		.then(function (result) {
+		  		
+			  		mainVm.movieDtlsList.push(result);
+					// console.log('mainVm.movieDtlsList', mainVm.movieDtlsList);
+			  
+				}, function (error) {
+				  	console.log('err', error.status);
+				});
+
+				movieDtlsService.getCreditsById(value)
+				.then(function (result) {
+					mainVm.parts[key].director = result.crew[0].name;
+				  	mainVm.movieCreditsList.push(result);
+					// console.log('mainVm.movieCreditsList', mainVm.movieCreditsList);
+				}, function (error) {
+				  console.log('err', error.status);
+				})
+		  	})(this[key]);
+		  	
+
+		}, mainVm.log);
+	  		// console.log('log', mainVm.log);
+	  		
+	  
+		}, function (error) {
+		  console.log('err', error.status);
+		});
+
+
+      	
+      	
+      	mainVm.getmovieDtls = function (id) {
+      	console.log('index', id);
+      	console.log('mainVm.movieDtlsList', mainVm.movieDtlsList[id]);
+      	console.log('mainVm.movieCreditsList', mainVm.movieCreditsList[id]);
+      	console.log('id', mainVm.parts[id].id);
+      	mainVm.movieDt.title = mainVm.movieDtlsList[id].title;
+      	mainVm.movieDt.posterPath = mainVm.movieDtlsList[id].poster_path;
+      	mainVm.movieDt.director = mainVm.movieCreditsList[id].crew[0].name;
+		mainVm.movieDt.stars = mainVm.movieCreditsList[id].cast[0].name+', '+
+								mainVm.movieCreditsList[id].cast[1].name+', '+
+								mainVm.movieCreditsList[id].cast[2].name+', '+
+								mainVm.movieCreditsList[id].cast[3].name;
+		mainVm.movieDt.writers ='';
+		angular.forEach(mainVm.movieCreditsList[id].crew, function(value, key) {	
+		  if (value.department == "Writing") {
+		  	mainVm.movieDt.writers += value.name+', ';	
+		  }
+		});
+		mainVm.movieDt.writers = mainVm.movieDt.writers.slice(0, -2);
+
+		mainVm.movieDt.overview = mainVm.movieDtlsList[id].overview;
+      }
+
+  };
--- app/scripts/services/movieDtls.service.js
+++ app/scripts/services/movieDtls.service.js
@@ -24,5 +24,37 @@
 
 				return defer.promise;
 		};
+
+		self.getMovieById = function (id) {
+			// defer object is created when you want to tell the caller of the function that whenever the response comes from 
+			// this call, I promise I will return back to you.
+			var defer = $q.defer();
+
+			$http
+				.get('https://api.themoviedb.org/3/movie/'+id+'?api_key='+apiKey)
+				.then(function (response) {
+					defer.resolve(response.data);//proimse made is successfull
+				}, function (error) {
+					defer.reject(error.status);//proimse made is unsuccessfull
+				});
+
+				return defer.promise;
+		};
+
+		self.getCreditsById = function (id) {
+			// defer object is created when you want to tell the caller of the function that whenever the response comes from 
+			// this call, I promise I will return back to you.
+			var defer = $q.defer();
+
+			$http
+				.get('https://api.themoviedb.org/3/movie/'+id+'/credits?api_key='+apiKey)
+				.then(function (response) {
+					defer.resolve(response.data);//proimse made is successfull
+				}, function (error) {
+					defer.reject(error.status);//proimse made is unsuccessfull
+				});
+
+				return defer.promise;
+		};
 	}
 })();
